a6f345b66dc0b86d495d94117801e869fabd4384
casperrr0706-maker <casperrr.0706@gmail.com>
2026-05-06 16:34:08 +0800

    add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  optimizeDeps: {
+    exclude: ['@imgly/background-removal'],
+  },
+  build: {
+    target: 'esnext',
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+})
